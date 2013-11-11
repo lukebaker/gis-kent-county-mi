3c0010657d500b268f1f3ad51059c33d2ee0a89c
Luke Baker <lukebaker@gmail.com>
2013-11-11 15:56:16 -0500

    update download only if-modified-since

--- download-convert.sh
+++ download-convert.sh
@@ -3,6 +3,7 @@
 set -eu
 set -x
 
+data_last_updated="$(git log -n 1 --format="%cd" data/)"
 if [ ! -d tmp ]; then
   mkdir tmp
 fi
@@ -13,7 +14,7 @@ while read url
 do
   downloadname=$(basename $url)
   noext=$(basename $downloadname .zip)
-  curl -Lsk $url -o "$downloadname"
+  curl -z "$data_last_updated" -Lsk $url -o "$downloadname"
   if [ -f $downloadname ]; then
     unzip -u $downloadname
     ogr2ogr -f GeoJSON -t_srs urn:ogc:def:crs:OGC:1.3:CRS84 $noext.geojson $noext.shp
